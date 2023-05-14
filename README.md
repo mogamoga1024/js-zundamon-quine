589033c08d5b199cb0f9dc3c338469c0f6c5db41
mogamoga <mogamoga.g1thu6@gmail.com>
2023-05-15 03:41:56 +0900

    dance

--- README.md
+++ README.md
@@ -8,3 +8,9 @@ JavaScriptで書かれたずんだもんのクワイン
 ## zundamon_yarimasune.js
 
 ![zundamon_yarimasune.js](./images/zundamon_yarimasune.gif)
+
+## zundamon_dance.js
+
+![zundamon_dance.js](./images/zundamon_dance.gif)
+
+元ネタ：https://www.youtube.com/watch?v=h0qzQ0QTmBg  
---
+++ zundamon_dance.js
@@ -0,0 +1,79 @@
+/*sorehabokunozundamotinanodasorehabokunozundamotinanodasorehabokunozundamotinanodasorehabokunozundamotinanodasorehabokunozundamotinanodasorehabokunozundamotinanodasorehabokunozundamotinanod
+asorehabokunozundamotinanodasorehabokunozundamotinanodasorehabokunozundamotinanodasorehabokunozundamotinanodasorehabokunozundamotinanodasorehabokunozundamotinanodasorehabokunozundamotinanoda
+sorehabokunozundamotinanodasorehabokunozundamotinanodasorehabokunozundamotinanodasorehabokunozundamotinanodasorehabokunozundamotinanodasorehabokunozundamotinanodasorehabokunozundamotinanodas
+orehabokunozundamotinanodasorehabokunozundamotinanodasorehabokunozundamotinanodasorehabokunozundamotinanodasorehabokunozundamotinanodasorehabokunozundamotinanodasorehabokunozundamotinanodaso
+rehabokunozundamotinanodasorehabokunozundamotinanodasorehabokunozundamotinanodasorehabokunozundamotinanodasorehabokunozundamotinanodasorehabokunozundamotinanodasorehabokunozundamotinanodasor
+ehabokunozundamotinanodasorehabokunozundamotinanodasorehabokunozundamotinanodasorehabokunozundamotinanodasorehabokunozundamotinanodasorehabokunozundamotinanodasorehabokunozundamotinanodasore
+habokunozundamotinanodasorehabokunozundamotinanodasorehab        okunozundamotinanodasorehabokunozundamotinanodasorehabokunozundamotinanodasorehabokunozundamotinanodasorehabokunozundamotinan
+odasorehabokunozundamotinanodasorehabokunozundamotina            nodasorehabokunozundamotinanodasorehabokunozundamotinanodasorehabokunozundamotinanodasorehabokunozundamotinanodasorehabokunoz
+undamotinanodasorehabokunozundamotinanodasorehabok                   unozundamotinanodasorehabokunozundamotinanodasorehabokunozundamotinanodasorehabokunozunda    motinanodasorehabokunozundam
+otinanodasorehabokunozundamotinanodasorehabokun    ozund                 amotinano*/i=1;s=`cc=String.fromCharCode;bq=cc(96);bs=cc(92);nl=cc(10);sp=cc(32);s         1="i="+(i+1)%18+";s="+bq+s
+;s2=bq+".replace(/("+bs+"s|"+bs+"n)+/g,'');ev   al      (s                 )";cm="";d=[10108,9996,10179,9964,9679,10335,10485,10429,10                               541][i%9];a1=["$$$$$$#1l8
+$#1hc$#1ej#2h4$#1b45h#2a9$#193262h#1nv$#175     24    2l                     #1f#12$#1675p#1a#15$#15bah9obr6a$#14a2a2b4#142cb426                                      28$#1392c273#1k3b222727$
+#1382c243#1o2d2468$#1382a242#1q2f2h$#13aa#       1f2i2                         e2f$#13#1y3j3c2e$#14z3v212m9f$#16v4v232#17$#1a                                         o232v242#12$#1dj252u262x
+$#1ak272t292q$#1722h265s2b2r$#1423h22cpe3           2s$#1233g2                 14a2n2b51                        2o23$#1025f                           2135bk          2476222n25$y35f21243a374
+523292432242g35$w45e22242d4422632d242223          2h          35           $v36                                        e2            2242f213d2f    23      33        212h55$t47e21242h2f2g242
+53i55$s47f21242h2e2h24262i55$s39e21242g         2f            2h       242                                                        52j           65  $s       39       e21242g2f2h24252i75$s2ae
+22232f2h2f25252h86$s2bd282c2k2d25252h95        $t            1b    d2a                                                            44             4o    454724        2ha5$#16d2#1y2hb4$#17c3sb
+t2g$#19a4q292r2f$#1b95p282q2d$#1f68o8m5        a$          #2    0#                                                              14               $#                 2j1$#27424232a25$#2562521
+2a27$#23224262b26$#2162224592425$#1y922          6642232bnj                                                   $#                  1w              92               5242322252fgq$#1v8252526282
+fet$#1u7262447282gbv$#1u62952a92g9x$#1u                                                                      42#                   122            g2              25x$#1u22#152f273g2d$#1w#192
+c2b673f$#1w#1b292i7j$#1v#1e8#1b$#1t#1h2#                                   1f$                               #1 s#                      1j4#1c$#1               s#1k6#18$#21#1b9#14$#1u8asly$#
+1xgq5mq$#2d35jxd$#1yfq2$#1yfdg$#1ydfg$#1zb                               hf$#                               1z   aj                                           e$$$$$$$$","$$$$$$#1l8$#1hc$#1ej
+#2h4$#1b45h#2a9$#193262h#1nv$#175242l#1f#12$#1                        67   5p                               #1    a#                                      15$#15bah9obr6a$#14a2a2b4#142cb42628
+$#1392c273#1k3b222727$#1382c243#1o2d2468$#1382a24                   2#     1q                              2f      2h                                 $#13aa#1f2i2e2f$#13#1y3j3c2e$#14z3v212m9
+f$#16v4v232#17$#1ao232v242#12$#1dj252u262x$#1a                    k2       72                             t2         92                          q$#1722h292s2b2r$#1423h2c2p2f2s$#1233g2f2n2h2
+o23$#1025f2i2k2l2n25$y35f2l27452p242g35$w45  e2                 p2      4226s                            23           2h                           35$v36e2c5c3#14212h55$t47e27565rbd3i55$s47f
+244d4m594a2i55$s39e233h3j4e562j65$s39e21  4j3                 g4  i452i75$s2ae                         213#133k352h86   $s                            2bd2#1v242h95$t1bd2#202ha5$#16d2#1y2hb4$
+#17c3#1w2g$#19a4r282r2f$#1b95q8t2d$#1f   68#                1i 5a$#          20                       #1           4$#2j 1$                        #2   7424232a25$#25625212a27$#23224262b26$#
+2162224592425$#1y9226642232bnj$#1w92  52423               22 252     fgq$#1v8252                    52    6282fet      $#  1u                       72     62447282gbv$#1u62952a92g9x$#1u42#12
+2g225x$#1u22#152f273g2d$#1w#192c2b   673f$               #1 w#    1b2          92i       7j$#     1v   #1         e8    #1b  $#    1t                #1h     2#1f$#1s#1j4#1c$#1s#1k6#18$#21#1b
+9#14$#1u8asly$#1xgq5mq$#2d35jxd$    #1yfq              2$  #1    yf             dg$#    1y  dfg$#1   zb             hf    $#  1z   aj                 e$$     $$$$$$","$$$$$$$$$$$#2ul$#2qt$ze
+hp3z$vj7#167a68$tl2#1a2745252d$   qk3#1d              2b  24    26               2f $o9             55               3#   1h2   c2 56                 h$n82     4222#1l3b2q$m7255#1r462o$l98#1
+12w5o$ke2#134#1j$kd2#14232#1a    $kb283v              25 3q    $k                 a2               72                12    v2     83#                  14$l8     27222u2c2#18$m626242u2f3#18$o
+326252t2j242t39$s6272r3m312u    67$s529               2p 3r    2v                 96              $r                 52    b2      n3                  t2uc5     $p72d2k2v2vf4$m92g2g37g92vi3$
+l3252i37683d552wl1$j4252n7b4   g442w$i32              72 b8    l3                p2               28                 m$    g4     28                   27385h     3o4a3h$f439233d4#153f3e$e43a
+232#1k3j2633$d54a213#1f6m24$   c55b212#1              b3 32    o2                2$               c4                 6c    2t     81                  27242q$     c47c2q2a285q$d37d2q2927222p$
+d38d2q4337242n$e1cb3#10662j$  vb7e824329              2c  $#   1s               f2                 c3               53     2$     #1                 r6462g52      $#1p12224242h38km$#1l727222
+f3gav$#1k624334e2p6v$#1d6232  6452e3t8p$#             1a        a2            22                    52             12     62     hd                 #1d$#17e3     14122253t2#1d$#16f2d4w4#1c$#
+15e2#1e232#1a$#15d2#1e23224#1 5$#16b2#1d2             52          82#1    0$#1                        i#1b     362b       2w    $#                 1i#19272d4     q$#1h#17372eae$#1h#133a2c$#1
+g#104a29$#1gw4b$#1gr47$#1gk6a$#1hb7g$#1rl$             #1                                                                      qf                 63$#249$#1y    f$#20c$$$$$$$","$$$$$$$$$#23f
+$#1x3cd$#1u42c2f2h$#1r62d2i2j$#1obd34f2m$#1            hw2                            42e2p$#1a#1                             55                g2r$#16#1u2u$#12#1z2v$z#243u$x#292t$w92#1y2t$v
+84#1jft$t8222#2o$s7242#2h$s5262#24$s4272#26$w          a2#2                          7$         v3                           53               2#29$u22324292#1y$s22524282#20$q32625242#24$oc27
+4#27$md363#2a$md243#2d$lg3#2h$ld3#2k$m125232#2k         $n257                         2#        2i                          $p             di#21$q92k3#1w$t23q2#1t$vu2#1q$uv5#1k$tv242#1g$tt27
+2nae$tr292l2a26$ul4c2l24#1kh$#1eio128#18p$#19p2j2c#      12u$#1aq                        3g4dvz$#                      1csf3          2goe3m$#1d#184kxp$#1d#1b3#23$#1c#1f2#20$#1c#18b#1x$#1b#1
+26c2#1u$#1bx4l3#1p$#1bs4t3#1j$#1ap3y7#1b$#1al4#10e#10$#1ai3#13nj$#1ag2#1                                        4$#1ad3y$#1ba3hi4$#1b92f3a29$#1c62d3g25$#1e32c2l21$#1g12a2p$#1vq$#1wp$#1zn$#1x
+34j$#1y64e$$$$$$$","$$$$$$#1sl$#1oq$#1kt$#1hw$#1fy$#1d#10$#1b#12$#1b#11$#1a#10#18c$#1a#1lho $#1a#1s7u$#1a#1w2y$#1c#2x$#1g#2v$#1964#2v$#1546#35$#1147#3a$x49#3a$u4a#39$r5a#37$p5a#2l3e$n6a#2p$m
+5b#2p22$l5a#2t23$k6a#2v22$k5a#2x22$k5a#2z$k5a#30$z#30$#10#2o2a$#11#2m2b$#13#2j2    c$    #1   56          4#     262d$#1746#232e$#1k#1y2f$#1oo515t2g$#1sh2b2#19$#11n9b2e2l3i$#10o25218221284eb
+g$#10n272c2bqe$#12l2a2a2c21$#157ag362d23$#1bv422e4c$#16#172e2g$#12#1c2f2e3m$y      #1     h2 g2          a2       t$y#1i2h262y$y#1j2i242#10$y12#1h2k212#12$y33#1g2k3#13$w2243#1e2n4z$u5273#1c2
+qdm$t8374#1a2#1n$tb384#173#1k$te494#143#1i$#1t#104#1f$#1yv6#1b$#28ka#17$#1y  13    69      be           #1      0$#1y44bur$#1y87dtc$#1yf$#1yh$#1yg$#1zd$#219$$$$$$$$","$$$$$#1o6$#1jb$#1gc$#1d
+k$#1aq$#18u$#16x$#15y#18q$#13#1qf#13$#12#1z5#18$#11#3e$#11#3f$#11#3f$#11b      3#  30    $#117         3#    34     $#1142#36$#15#36$y#3b$u#3c$s35#2m2c$r27#2o$q27#2q$p36#2u$q33#2z$q#38$r#322
+5$t#3135$v#3054$v#3065$v#3075$v#2za4$wa2#2lc3$wb2#2kd2$x67#2hg1$#1ch7#         1q  $#1fc2      82#1  l$   #1           3e34872#1h$#12j2a252#10                   39$#12h2f222ub3$#12e2i8i$#12c
+2i29$#13a2i2i27$#1572ilc$#1di2y$#1cjh92a$#1bh4h462822$#1bf2q242729$#         1a     #1    a3   22  62     c$               #1a#1d22232f$#1a                          #1e22212h$#1a#1f232h$#1a#
+1g233c$#1b#1f24$#1c#1d26$#1d#1b28$#1f#172b$#1j#103e$#1ns4e$#2f1$#1v        w$     #1     zh      $#        1w               4d128$#1xi26$#                             1yg25$#1zf23$#20c$#229$
+$$$$$$$","$$$$$$#1r8$#1oj$#1lqgd$#1kv6#13$#1jx2#17$#1jt4#19$#1j7a#       1p      $#    1k15       #1        z$                #1l#23$#1f#                               29$#1c#2a$#18#2c$#16#2
+d$#13#2b$#11#2h$z#2l$x#2b2b$w#2c3b$v#2c212b$v#2b214b$v#29712b$v#28      55         2b$v#  26852a$v#2         42                134242a$v                                 #1v262126242a$w#1u242
+137233a$y#1r232238224a$z#1q2123472529$#11#1p27262627$#13#1n2626262    5$                                      #1                6#  1k262                                 527$#1e#1d257a$#1773
+#1a2m$#152462#192k$yd252#192i$tk232wg366$pp9u2e2523$mqmfd62423$jrf  9f                                         d$               hs       gf2                p$             ftd42c2r$dpg9292t$d
+n2g56bt$dl2n4b2r$dk2s3a2n$dk2v2a$el2v2s$g#1h2q$i#1g2o$j#1g2o$l#1e2p$                                             n#            1c           2r$p#1       92t               $r#152x$vz3z$u15q4#
+12$t7ba5#16$sja49y$sgecp7$tbly$#1wm$$#1zi$#20g$#21e$#23a$$$$$$$$","$                                               $$         $#                  1d5$#1a                   a$#17d$#14j$#12q$#
+10v$zd5g$yc252g$xc252i$xe5k2lkb$xka#128p64$yg2a2#152#13$ze2b2#182#1                                                  2$#10d2a                                               2#1j6p$#11ea#1k252
+75c$#12#262824242a$#14#132#1029236b$#11#13222#10372k$z#13242u277l                                                     $x                                                   #13262v2w$v#13cv382
+i$u52u573v21292b$t42m26a62u2229$s33m2548342t232b$r34m225c242s612                                                       c$q3                                                4m8g232q244f$q25m22
+4g233o363b23$q33m224h243m22253b33$q42m21212h27442a25253b43$qs212                                                        12g2c4                                            22538243c53$sq242f2k
+6b23212c54$wm242e2k2f24222c63$yl242b3k2g24212d73$zk26ao2f24222d84$#11k2#1                                               32d25222d                                        85$#14j2#122a35222e94
+$#18i2#1296242e95$#1qh2#152ea5$#1lo3#103eb4$#1r#1j3ec3$#1q25#185e$        #1ae27216r                            $#1152g2827$tc2g292a2                                  22326$pg2g2a2b452528$ni
+2g2b262723242b$lm3d2c7622632e$kj53eg25212321212h$jh3fca252224222i$j#1                26e66642g$j#123#152e$jz3#1     a$jx2#1e$kv2#1g$lt2#1j                          $oo3#1l$sh4#1o$#1e#1o$#1p#
+1d$#1l5c#10$#1ofka$#1rx$#23p$#1ua91$#1vi27$#1xg26$#20d24$#22a$$$$$$$$","$$$$$$#1b9$#1   7e$#1                   4j#207$#12s#1ua$#10a5j#1rc$z9242k             #1go$z924255c#1bt$za4535274wbm75
+$zg292#1e3m2624$ze2a2#1j396586$#10d292#1v263g$#11d9#1y292e$#12#2j2b2c$               #14#2j292c$#18x2w2kad$#1fn  212x3#14$#1co232x212#11$#18q252y222g2f$#1442m236x242i28$#1045l752w442i22$y36l
+4b3t23412j22$w37k44ds275i22$u38j433b3p674j22$t38g21342e4m26263j22$r39g               642g5i2a253j2                2$q39h542h2234273d253j22$p3ah542h254221312f253i22$p2bh452h2i2g253i21$p2bh452
+h2h2h253i21$p2bh21242g2i2h24212h21$p2bi272e2j2g25212h$p2ci272c2l2f2522             2f$q1ci2aap2c2b                2f$#14i2#1abe2e$#15h2#1z2c$#17g2ncy3b$#1ae2m282x39$#1dc4k9x55$#1k68#1g$#25#1
+3$#215$#1y92426232822$#1xa26222629$#1wa29282926$#1t328263236282c$#1kd23           22612223782f$#1kf               2128234325232g$#1kh372444263h$#1kh28242223276a$#1nc2b644b23$#1z#1322$#1x#17$
+#17db#1a$#14j7#1c$#12o2#1e$#12n2#1g$#12m2#1i$#14k2#1i$#15ka#1a$#18i27dx          $#1ce4i$#20#15$#1y2              $#20fd8$#21ef5$#22d$#24b$$$$$$$"][i%9];if(i===16){d=10369;a1="$$$#1d4$#1a9$#
+17d$#14h$#12o$#10t$zc4h$yb242h$xb252i$xc6l2i$xk7b2qbl83$xi282#144#12$yf2a2#172#13$yf2a2#1i5q$#10ea#1j25284d$#11#252825232b$#13#132z2824242a$#12#133#1127254c$#10#12312v268l$x#13342v2x$w#12722
+v382k$u52veu21282e$t42v3b2u222924$s33m25c62t232a$r34l244b242s242c$q25m215e242q262e$p34m413g242p272f$p33n224g243n292b24$p32o214h24212j2c2b34$ps215h2948232f2b44$qr21212g2e822i2c53$to242f2#1a2b
+64$wa65252d2#1b2c73$x6374125293pbc3c74$z22f377q4a392d84$#11k2#1j272d94$#11m2der342e95$#11o2a2c2q342e95$#12o282d2v3ea5$#12q272b2v2ec4$#14p285x6e$#16o213ycc$#18m4291n9$xc2j21283123282f$sk4d232
+a26262f$otc6283134252d$mu9a2626752b$kn43da662327$jj46ia26432131$ih3gbb24232422$i#126g4664$i#123#16$iz3#1b$jw2#1f$ku2#1h$ls2#1k$on2#1n$sg4#1p$#1e#1o$#1p#1d$#1l4c#11$#1nem8$#1rw$#22q$#1t9b1$#1
+uj25$#1xg24$#1zd23$#22a$$$$$$$$";}d-=s1.length+s2.length;if(d>3){cm="/*";for(j=0;j<d-4;j++)cm+="sorehabokunozundamotinanoda"[j%27];cm+="*/"}s=cm+s1+s2;a2=[];j=0;while(j<a1.length){c=a1[j];if
+(c==="$"){a2.push(-1);}else{if(c==="#"){a2.push(parseInt(a1[++j]+a1[++j],36));}else{a2.push(parseInt(c,36));}}j++;}spf=0;t="";st=cn=0;a2.forEach(e=>{if(e<0){d=190-cn;t+=s.slice(st,st+d);st+=
+d;t+=nl;spf=cn=0;}else{if(spf){t+=sp.repeat(e);}else{t+=s.slice(st,st+e);st+=e;}spf=!spf;cn+=e;}});console.clear();console.log(t);setTimeout(()=>eval(s),500)`.replace(/(\s|\n)+/g,'');eval(s)
